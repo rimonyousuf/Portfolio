9432e9fda7e51bbb2095c659d900fbc556641476
Rimon <rimonyousuf00@gmail.com>
2024-06-08 21:46:18 +0600

    First commit navbar has been added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css';
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <img className='logo' src={logo} alt="" />
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>Connect</li>
+            </ul>
+            <div className="nav-connect">Connect With Me</div>
+        </div>
+    );
+};
+
+export default Navbar;
