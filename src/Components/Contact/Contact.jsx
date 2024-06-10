c8937d97d4161ea5357db63e2e3a591928db319e
Rimon <rimonyousuf00@gmail.com>
2024-06-10 22:22:21 +0600

    Web3form has been added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,8 +3,43 @@ import theme_pattern from '../../assets/theme_pattern.svg';
 import mail_icon from '../../assets/mail_icon.svg';
 import location_icon from '../../assets/location_icon.svg';
 import call_icon from '../../assets/call_icon.svg';
+import Swal from 'sweetalert2';
+import { useRef } from 'react';
 
 const Contact = () => {
+
+    const formRef = useRef(null)
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", import.meta.env.VITE_FORM_ACCESS_KEY);
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Your message has been sent successfully",
+                showConfirmButton: false,
+                timer: 1500
+              });
+              formRef.current.reset();
+        }
+    };
+
     return (
         <div id='Contact' className='contact'>
             <div className="contact-title">
@@ -30,7 +65,7 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <form className='contact-right'>
+                <form onSubmit={onSubmit} className='contact-right' ref={formRef}>
                     <label htmlFor="">Your Name</label>
                     <input type="text" placeholder='Enter your name' name="name" />
 
